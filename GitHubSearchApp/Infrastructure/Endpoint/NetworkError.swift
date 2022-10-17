44c32132d91c3c413eff8a1866733e1ddca6f641
chminipark <chminipark@gmail.com>
2022-10-17 19:29:55 +0900

    [Feat] : NetworkError 케이스 추가
    Related to: #6

--- GitHubSearchApp/Infrastructure/Endpoint/NetworkError.swift
+++ GitHubSearchApp/Infrastructure/Endpoint/NetworkError.swift
@@ -7,12 +7,29 @@
 
 import Foundation
 
-enum NetworkError: String, Error {
+enum NetworkError: Error {
     case urlComponentError
     case queryEncodingError
     case makeURLError
+    case responseError(Error)
+    case unknownError(Error)
+    case statusCodeError(Int)
     
     var description: String {
-        "😡😡😡😡😡" + self.rawValue
+        let base = "😡😡😡😡😡"
+        switch self {
+        case .urlComponentError:
+            return base + "urlComponentError"
+        case .queryEncodingError:
+            return base + "queryEncodingError"
+        case .makeURLError:
+            return base + "makeURLError"
+        case .responseError(let error):
+            return base + "responseError : \(error.localizedDescription)"
+        case .unknownError(let error):
+            return base + "unknownError : \(error.localizedDescription)"
+        case .statusCodeError(let code):
+            return base + "statusCodeError : \(code)"
+        }
     }
 }
--- GitHubSearchAppTests/GitHubSearchAppTests.swift
+++ GitHubSearchAppTests/GitHubSearchAppTests.swift
@@ -9,31 +9,46 @@ import XCTest
 @testable import GitHubSearchApp
 import RxSwift
 
-//
-//protocol Provider {
-//    func request<E: RequestResponsable, R: Decodable>(endpoint: E) -> Single<Result<>> where E.Response == R
-//}
-//
+protocol Provider {
+    func request<E: RequestResponsable, R: Decodable>(endpoint: E) -> Single<R> where E.Response == R
+}
+
 //class ProviderImpl: Provider {
-//
 //    let session = URLSession.shared
 //
 //    func request<E: RequestResponsable, R: Decodable>(endpoint: E) -> Single<R> where E.Response == R {
-//
-//        do {
-//            let urlRequest = try endpoint.makeURLRequest()
-//        } catch {
-//            return .create { emitter in
-//                emitter(.)
-//            }
-//        }
-//
 //        return Single.create { emitter in
-//
+//            do {
+//                let urlRequest = try endpoint.makeURLRequest()
+//                session.dataTask(with: urlRequest) { data, response, error in
+//                    <#code#>
+//                }
+//                
+//            } catch {
+//                emitter(.failure(error))
+//            }
+//            
+//            
 //            return Disposables.create()
 //        }
 //    }
+//    
+//    func checkError(data: Data?, response: URLResponse?, error: Error?, completion: @escaping (Result<SearchRepoResponseDTO, Error>) -> Void) {
+//        if let error = error {
+//            completion(.failure(NetworkError.unknownError(error)))
+//            return
+//        }
+//        
+//        guard let response = response else {
+//            completion(.failure(<#T##Error#>))
+//        }
+//
+//        
+//        
+//    }
 //}
+
+
 
 class GitHubSearchAppTests: XCTestCase {
     
