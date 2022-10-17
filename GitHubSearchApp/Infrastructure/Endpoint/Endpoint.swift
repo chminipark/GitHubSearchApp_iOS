d30c968b20a7be3b6ae8433d4e278738082900ba
chminipark <chminipark@gmail.com>
2022-10-17 14:32:24 +0900

    [Feat] : makeURLRequest 함수 생성
    Related to: #6

--- GitHubSearchApp/Infrastructure/Endpoint/Endpoint.swift
+++ GitHubSearchApp/Infrastructure/Endpoint/Endpoint.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 struct QueryParameter: Encodable {
-    
+    let q: String
+    let per_page: Int
+    let page: Int
 }
 
 protocol Requestable {
@@ -17,4 +19,49 @@ protocol Requestable {
     var queryParameter: QueryParameter { get }
 }
 
+enum NetworkError: String, Error {
+    case urlComponentError
+    case queryEncodingError
+    case makeURLError
+    
+    var description: String {
+        "😡😡😡😡😡" + self.rawValue
+    }
+}
 
+extension Encodable {
+    func toDictionary() throws -> [String : Any] {
+        let data = try JSONEncoder().encode(self)
+        let jsonObject = try JSONSerialization.jsonObject(with: data)
+        return jsonObject as! [String : Any]
+    }
+}
+
+extension Requestable {
+    func makeURLRequest() throws -> URLRequest {
+        let urlString = baseURL + path
+        guard var urlComponent = URLComponents(string: urlString) else {
+            throw NetworkError.urlComponentError
+        }
+        
+        guard let queryDictionary = try? queryParameter.toDictionary() else {
+            throw NetworkError.queryEncodingError
+        }
+        
+        var queryItemList = [URLQueryItem]()
+        queryDictionary.forEach { (key, value) in
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            queryItemList.append(queryItem)
+        }
+        
+        if !queryItemList.isEmpty {
+            urlComponent.queryItems = queryItemList
+        }
+        
+        guard let url = urlComponent.url else {
+            throw NetworkError.makeURLError
+        }
+        
+        return URLRequest(url: url)
+    }
+}
