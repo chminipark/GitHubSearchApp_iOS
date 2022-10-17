d9003403e8b896eb2765f0091aea94dd7a487e4b
chminipark <chminipark@gmail.com>
2022-10-17 14:50:24 +0900

    [Feat] : struct Endpoint 생성
    Related to: #6

--- GitHubSearchApp/Infrastructure/Endpoint/Endpoint.swift
+++ GitHubSearchApp/Infrastructure/Endpoint/Endpoint.swift
@@ -7,3 +7,16 @@
 
 import Foundation
 
+struct Endpoint: Requestable {
+    var baseURL: String
+    var path: String
+    var queryParameter: QueryParameter
+    
+    init(baseURL: String,
+         path: String,
+         queryParameter: QueryParameter) {
+        self.baseURL = baseURL
+        self.path = path
+        self.queryParameter = queryParameter
+    }
+}
