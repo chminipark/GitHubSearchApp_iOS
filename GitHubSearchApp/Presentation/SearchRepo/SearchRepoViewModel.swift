39dd295e4c828d26b0b76c8f440095098e73bd8d
chminipark <chminipark@gmail.com>
2022-10-19 20:18:02 +0900

    [Feat] : @propertyWrapper Property 생성
    Related to: #12

---
+++ GitHubSearchApp/Presentation/SearchRepo/SearchRepoViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  SearchRepoViewModel.swift
+//  GitHubSearchApp
+//
+//  Created by chmini on 2022/10/19.
+//
+
+import Foundation
+
+class SearchRepoViewModel {
+}
---
+++ GitHubSearchApp/Support/Property.swift
@@ -0,0 +1,40 @@
+//
+//  Property.swift
+//  GitHubSearchApp
+//
+//  Created by chmini on 2022/10/19.
+//
+
+import Foundation
+import RxCocoa
+
+@propertyWrapper
+struct Property<Value> {
+    let lock = NSLock()
+    var relay: BehaviorRelay<Value>
+    
+    init(_ value: Value) {
+        self.relay = BehaviorRelay<Value>(value: value)
+    }
+    
+    var wrappedValue: Value {
+        get { load() }
+        set { store(newValue) }
+    }
+    
+    var projectedValue: BehaviorRelay<Value> {
+        return self.relay
+    }
+    
+    func load() -> Value {
+        lock.lock()
+        defer { lock.unlock() }
+        return self.relay.value
+    }
+    
+    func store(_ value: Value) {
+        lock.lock()
+        defer { lock.unlock() }
+        self.relay.accept(value)
+    }
+}
