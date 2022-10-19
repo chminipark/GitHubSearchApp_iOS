8f9d5bc265d9faffa329a61068ee5bafdf00cc7c
chminipark <chminipark@gmail.com>
2022-10-19 21:50:57 +0900

    [Feat] : SearchRepoViewModel Input, Output 테스트
    서치바 텍스트 콘솔창에 띄우기
    
    Related to: #12

--- GitHubSearchApp/Presentation/SearchRepo/SearchRepoViewController.swift
+++ GitHubSearchApp/Presentation/SearchRepo/SearchRepoViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class SearchRepoViewController: UIViewController {
-    let numberList = (1...20).map { $0 }
+    let searchRepoViewModel = SearchRepoViewModel()
     
     private let searchBar: UISearchController = {
         let sb = UISearchController()
@@ -30,6 +32,7 @@ final class SearchRepoViewController: UIViewController {
         configureUI()
         setupSearchBar()
         setupTableView()
+        bindToViewModel()
     }
     
     private func configureUI() {
@@ -44,38 +47,51 @@ final class SearchRepoViewController: UIViewController {
         ])
     }
     
+    let disposeBag = DisposeBag()
+    
+    private func bindToViewModel() {
+        let input = SearchRepoViewModel.Input(searchBarText: searchBar.searchBar.rx.text.orEmpty.asDriver())
+        let output = searchRepoViewModel.transform(input, disposeBag: disposeBag)
+        
+        output.$searchBarText
+            .subscribe(onNext: { text in
+                print(text)
+            })
+            .disposed(by: disposeBag)
+    }
+    
     private func setupTableView() {
-        self.tableView.dataSource = self
+//        self.tableView.dataSource = self
     }
     
     private func setupSearchBar() {
         navigationItem.searchController = searchBar
-        searchBar.searchBar.delegate = self
+//        searchBar.searchBar.delegate = self
     }
 }
 
-extension SearchRepoViewController: UISearchBarDelegate {
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        guard let text = searchBar.text else {
-            return
-        }
-        print(text)
-    }
-}
-
-extension SearchRepoViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberList.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        var content = cell.defaultContentConfiguration()
-        content.text = "\(numberList[indexPath.row])"
-        cell.contentConfiguration = content
-        
-        cell.separatorInset = .zero
-        return cell
-    }
-}
+//extension SearchRepoViewController: UISearchBarDelegate {
+//    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+//        guard let text = searchBar.text else {
+//            return
+//        }
+//        print(text)
+//    }
+//}
+//
+//extension SearchRepoViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return numberList.count
+//    }
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        var content = cell.defaultContentConfiguration()
+//        content.text = "\(numberList[indexPath.row])"
+//        cell.contentConfiguration = content
+//
+//        cell.separatorInset = .zero
+//        return cell
+//    }
+//}
 
 
--- GitHubSearchApp/Presentation/SearchRepo/SearchRepoViewModel.swift
+++ GitHubSearchApp/Presentation/SearchRepo/SearchRepoViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 protocol ViewModel {
     associatedtype Input
@@ -17,14 +18,20 @@ protocol ViewModel {
 
 class SearchRepoViewModel: ViewModel {
     struct Input {
+        let searchBarText: Driver<String>
     }
     
     struct Output {
-        @Property var repoList = [Repository]()
+//        @Property var repoList = [Repository]()
+        @Property var searchBarText = ""
     }
     
     func transform(_ input: Input, disposeBag: DisposeBag) -> Output {
-        var output = Output()
+        let output = Output()
+        
+        input.searchBarText
+            .drive(output.$searchBarText)
+            .disposed(by: disposeBag)
         
         return output
     }
