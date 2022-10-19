e9af7cedabeb52b3418fcd0c6e6153f0d8d75aae
chminipark <chminipark@gmail.com>
2022-10-19 14:24:49 +0900

    [Test] : MockURLSession 구현, json Mock Data 추가
    Related to: #10

--- GitHubSearchAppTests/GitHubSearchAppTests.swift
+++ GitHubSearchAppTests/GitHubSearchAppTests.swift
@@ -17,46 +17,64 @@ class MockURLSessionDataTask: URLSessionDataTask {
     }
 }
 
-//class MockURLSession: URLSessionable {
-//    
-//    var isFail: Bool
-//    
-//    init(isFail: Bool) {
-//        self.isFail = isFail
-//    }
-//    
-//    var mockURLSessionDataTask: URLSessionDataTask?
-//    
-//    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-//
-//        let mockSessionDataTask = MockURLSessionDataTask()
-        
-//        guard let url = request.url else {
-//            isFail = true
-//            return
-//        }
-                
-//        let successResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
-//        let failureResponse = HTTPURLResponse(url: url, statusCode: 500, httpVersion: nil, headerFields: nil)
-//
-//        if isFail {
-//            mockSessionDataTask.resumeDidCall = { completionHandler(nil, failureResponse, nil) }
-//        } else {
-//            mockSessionDataTask.resumeDidCall = { completionHandler(nil, successResponse, nil) }
-//        }
-//
-//        self.mockURLSessionDataTask = mockSessionDataTask
-//        return mockSessionDataTask
-//    }
-//}
-
-class GitHubSearchAppTests: XCTestCase {
+class MockURLSession: URLSessionable {
     
-    var sut: Provider!
+    var isFail: Bool
     
-    override func setUpWithError() throws {
-        
+    init(isFail: Bool) {
+        self.isFail = isFail
     }
     
+    var mockURLSessionDataTask: URLSessionDataTask?
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        let mockSessionDataTask = MockURLSessionDataTask()
 
+        let data = MockData.searchRepoResponseDTO
+        
+        let successResponse = HTTPURLResponse(url: request.url!,
+                                              statusCode: 200,
+                                              httpVersion: nil,
+                                              headerFields: nil)
+        let failureResponse = HTTPURLResponse(url: request.url!,
+                                              statusCode: 500,
+                                              httpVersion: nil,
+                                              headerFields: nil)
+
+        if isFail {
+            mockSessionDataTask.resumeDidCall = { completionHandler(nil, failureResponse, nil) }
+        } else {
+            mockSessionDataTask.resumeDidCall = { completionHandler(data, successResponse, nil) }
+        }
+
+        self.mockURLSessionDataTask = mockSessionDataTask
+        return mockSessionDataTask
+    }
+}
+
+class GitHubSearchAppTests: XCTestCase {
+    var sut: Provider!
+    var disposeBag: DisposeBag!
+    
+    override func setUpWithError() throws {
+        self.sut = ProviderImpl(session: MockURLSession(isFail: false))
+        self.disposeBag = DisposeBag()
+    }
+    
+    func test_requestSuccess() {
+        // given
+//        let expectation = XCTestExpectation()
+        let endpoint = APIEndpoints.searchRepo(with: SearchRepoRequestDTO(q: ""))
+                
+        // when
+        sut.request(endpoint: endpoint)
+            .subscribe(onSuccess: { data in
+                XCTAssertEqual(40, data.totalCount, "efefe")
+//                expectation.fulfill()
+            }, onFailure: { error in
+                XCTFail(error.localizedDescription)
+            })
+            .disposed(by: disposeBag)
+//        wait(for: [expectation], timeout: 5)
+    }
 }
