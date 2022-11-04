a1abacbf23d90bcb8e292d4a10a5b4ded4b16868
chminipark <chminipark@gmail.com>
2022-11-04 16:46:44 +0900

    [Feat] : Create NSManagedObject SubClass
    Related to: #27

---
+++ RepoModel+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  RepoModel+CoreDataProperties.swift
+//  GitHubSearchApp
+//
+//  Created by chmini on 2022/11/04.
+//
+//
+
+import Foundation
+import CoreData
+
+extension RepoModel {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<RepoModel> {
+        return NSFetchRequest<RepoModel>(entityName: "RepoModel")
+    }
+
+    @NSManaged public var urlString: String?
+    @NSManaged public var starCount: Int16
+    @NSManaged public var repoDescription: String?
+    @NSManaged public var name: String?
+    @NSManaged public var id: UUID?
+
+}
+
+extension RepoModel : Identifiable {
+
+}
